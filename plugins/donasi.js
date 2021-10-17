4c9dc7b347a8d10ef7fa12e3924241d55ea6259f
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-10-18 05:26:17 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,18 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+
+├ Dana,Gopay,Linkaja (082387704390)
+├ Pulsa 
+├ three wa.me/6289601145546
+├ telkomsel wa.me/6282387704390
+├ https://saweria.co/Doraemond
+├
+├ _saya harap kamu menyukai bot saya
+├ dan jangan lupa untuk mensupport saya
+├ karena saya sangat menyukai Dora :D_
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '© Doraemond', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i

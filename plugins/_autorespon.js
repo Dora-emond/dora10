fc13bec5ca99f689ed468e4b8099057b86577915
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-10-18 23:56:40 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -14,7 +14,7 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'stikerin tidak aktif' : banned ? 'kamu dibanned' : 'stikerin disini',
+                isBanned ? 'stikerin tidak aktif' : banned ? 'kamu dibanned' : 'Dora disini',
                 '© stikerin',
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
